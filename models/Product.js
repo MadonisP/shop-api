733159c760e29673f4a3726418097d7e75ebe042
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-07-29 11:01:43 +0300

    product instock feature added

--- models/Product.js
+++ models/Product.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
     title: { type: String, required: true, unique: true },
-    desc: { type: String, required: true},
+    desc: { type: String, required: true },
     img: { type: String, required: true },
     categories: { type: Array, required: true },
-    size: { type: String},
-    color: { type: String},
-    price: { type: String},
+    size: { type: Array },
+    color: { type: Array },
+    price: { type: String },
+    inStock: { type: Boolean, default: true },
 },
     { timestamps: true }
 );
